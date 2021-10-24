fb05db2e48f816fdb3fc437fe8dd80fcacabc3c6
lucaslpa12345 <lucaslpa12345@gmail.com>
2021-10-23 23:36:57 -0300

    add Delete All clients

--- src/components/DeleteAll/index.tsx
+++ src/components/DeleteAll/index.tsx
@@ -1,20 +1,54 @@
 import { TrashFill } from '@styled-icons/bootstrap'
+import { useSession } from 'next-auth/react'
 import * as S from './style'
 import { useSelectContext } from '../../contexts/select'
+import { useClientsContext } from '../../contexts/Clients'
+
 import { Button } from '../Button'
+import { ClientDeleteMany } from '../../api/clients'
 
 export type props = {
   hidden?: boolean
 }
 
-export const DeleteAll = ({  hidden = false }: props) => {
+export const DeleteAll = ({ hidden = false }: props) => {
   const { Selected, setSelected } = useSelectContext()
+  const { state, setClientsContext } = useClientsContext()
+  const { data } = useSession()
+
+  function handleDeleteFromState() {
+    const oldData = state.ClientsData
+    const newData = oldData.filter((e) => {
+      const find = Selected.find((id) => id === e.id)
+      if (find) return null
+      return find
+    })
+    if (newData) {
+      setClientsContext!({ ...state, ClientsData: newData! })
+    }
+  }
+
+  async function handleDeleteAll() {
+    if (data && data.accessToken) {
+      const DeleteAllResponse = await ClientDeleteMany(
+        Selected,
+        data.accessToken
+      )
+      if (DeleteAllResponse) {
+        handleDeleteFromState()
+      }
+    }
+  }
 
   return (
     <S.Wrapper hidden={hidden} data-testid="deleteAll">
       <strong>{Selected?.length} Selecionados</strong>
 
-      <Button Icon={<TrashFill width={25} />} label="Deletar todos" />
+      <Button
+        onClick={() => handleDeleteAll()}
+        Icon={<TrashFill width={25} />}
+        label="Deletar todos"
+      />
     </S.Wrapper>
   )
 }
