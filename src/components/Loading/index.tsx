0ec8ea56e568094a3fa59ad58173e99ffd491590
lucaslpa12345 <lucaslpa12345@gmail.com>
2021-10-29 00:47:38 -0300

    change Loading icon

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -2,6 +2,6 @@ import * as S from './style'
 
 export const Loading = () => (
   <S.Wrapper>
-    <img src="/loading.gif" alt='carregando'/>
+    <S.LoadingIcon />
   </S.Wrapper>
 )
--- src/components/Loading/style.tsx
+++ src/components/Loading/style.tsx
@@ -1,4 +1,5 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
+import { Spinner9 } from '@styled-icons/icomoon'
 
 export const Wrapper = styled.div`
   width: 100%;
@@ -6,4 +7,18 @@ export const Wrapper = styled.div`
   display: flex;
   justify-content: center !important;
   align-items: center;
+`
+const spin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`
+
+export const LoadingIcon = styled(Spinner9)`
+  animation: 2s linear ${spin} infinite;
+  width: 3rem;
+  fill: blue;
 `
