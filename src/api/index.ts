f971f4b90e813da09ee60e000360bfbcfe483f7a
lucaslpa12345 <lucaslpa12345@gmail.com>
2021-11-01 19:37:27 -0300

    add API_URL to env

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const Api = axios.create({
-  baseURL: `http://localhost:2000`,
+  baseURL: `${process.env.API_URL}`,
   responseType: 'json',
   validateStatus: () => true, // I'm always returning true, you may want to do it depending on the status received
 })
--- src/services/administrator/administrator.ts
+++ src/services/administrator/administrator.ts
@@ -11,16 +11,22 @@ export const LoginAdministrator = async (login: {
   username: string
   password: string
 }): Promise<Return> => {
-  const response = await Api.post<httpResponse>('/administrator/login', login)
+  try {
+    const response = await Api.post<httpResponse>('/administrator/login', login)
+    if (response.data.status === 400)
+      return { login: false, message: 'Admininistrador inexistente' }
+    if (response.data.status !== 200 || !response.data.data.token)
+      return { login: false, message: 'Algo inesperado aconteceu' }
 
-  if (response.data.status === 400)
-    return { login: false, message: 'Admininistrador inexistente' }
-  if (response.data.status !== 200 || !response.data.data.token)
-    return { login: false, message: 'Algo inesperado aconteceu' }
-
-  return {
-    login: true,
-    message: 'Login bem sucedido',
-    token: response.data.data.token,
+    return {
+      login: true,
+      message: 'Login bem sucedido',
+      token: response.data.data.token,
+    }
+  } catch (err) {
+    return {
+      login: false,
+      message: 'Algo de errado aconteceu',
+    }
   }
 }
