4ba3c2b23385402f2b5ead80383be7c50a098ec2
lucaslpa12345 <lucaslpa12345@gmail.com>
2021-11-01 17:55:04 -0300

    update next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  typescript: { ignoreBuildErrors: true },
   images: {
     domains: ['cdn-icons-png.flaticon.com'],
   },
